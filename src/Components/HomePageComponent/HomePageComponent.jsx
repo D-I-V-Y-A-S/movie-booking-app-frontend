8bf3bab6407b18c8634d525ada0b974f61b4e4b8
D-I-V-Y-A-S <divyaselvam1405@gmail.com>
2024-06-20 21:07:05 +0530

    search feature implemented

--- src/Components/HomePageComponent/HomePageComponent.jsx
+++ src/Components/HomePageComponent/HomePageComponent.jsx
@@ -6,33 +6,43 @@ import { Link, Route } from 'react-router-dom'
 import UserLoginComponent from '../UserLoginComponent/UserLoginComponent'
 
 const HomePageComponent = () => {
-
   const [movieData, setMovieData] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+  const [filteredMovies, setFilteredMovies] = useState([])
+
   useEffect(() => {
     axios.get('http://localhost:3500/api/v1/movie')
-      .then(response => {setMovieData(response.data)
+      .then(response => {
+        setMovieData(response.data)
         console.log(response.data)
       })
       .catch((error) => { alert(`Status : ${error.response.status} - ${error.response.data.message}`) })
-  },[movieData])
+  }, [])
+
+  const searchHandler = (event) => {
+    setSearchInput(event.target.value)
+    axios.get(`http://localhost:3500/api/v1/movie/${searchInput}`)
+      .then(response=>setFilteredMovies(response.data))
+      .catch((error) => { alert(`Status : ${error.response.status} - ${error.response.data.message}`)})
+  }
 
   return (
-   <div className='movies'>
+    <div className='movies'>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
       <nav className="navbar">
         <div className="logo">
           <i className="bi bi-film"></i>
           <a href="#">FILM FIESTA</a>
         </div>
-       
-       
+
+
         <input
           type="text"
-          name="search"
+          name="searchInput"
           placeholder="search by movieName"
-        // value={searchInput}
-        >
-        </input>
+          value={searchInput}
+          onChange={searchHandler}
+        />
 
         <div className="menu">
           <div className="menu-links">
@@ -44,16 +54,24 @@ const HomePageComponent = () => {
           <i className="fa-solid fa-bars"></i>
         </div>
       </nav>
+
       <div className='box'>
-<div className='container'>
-      {movieData && movieData.map((item,index) => (
-        <div key={index} className='items'>
-          <DisplayComponent item={item} />
-        </div>  
-      ))}
+        <div className='container'>
+          {filteredMovies.length===0 ?
+          (movieData && movieData.map((item, index) => (
+            <div key={index} className='items'>
+              <DisplayComponent item={item} />
+            </div>
+          ))):
+          (filteredMovies && filteredMovies.map((item, index) => (
+            <div key={index} className='items'>
+              <DisplayComponent item={item} />
+            </div>
+          ))) }
         </div>
-        </div>
-        </div>
+      </div>
+    </div>
+
   )
 }
 
